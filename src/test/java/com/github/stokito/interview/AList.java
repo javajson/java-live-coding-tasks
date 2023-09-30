a10b916e9056d43ba0acf3a03bd8d42b1012518c
Alexey Belyaev <>
2023-09-30 19:19:56 +0300

    3 test fixed

--- src/test/java/com/github/stokito/interview/AList.java
+++ src/test/java/com/github/stokito/interview/AList.java
@@ -1,24 +1,43 @@
 package com.github.stokito.interview;
 
-public class AList  {
-    public AList() {
+import java.rmi.*;
+import java.util.*;
 
+public class AList {
+
+    private int initialCapacity;
+    private int size;
+    private static final int CAPACITY = 10;
+    String[] elements;
+
+    public AList() {
+        elements = new String[0];
+        size = 0;
     }
 
     public AList(int initialCapacity) {
-
+        if (initialCapacity < 0) {
+            throw new IllegalArgumentException("Illegal Capacity: " +
+                    initialCapacity);
+        }
+        elements = new String[initialCapacity];
+        size = initialCapacity;
     }
 
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
     }
 
     public int size() {
-        return -1;
+        return size;
     }
 
     public void add(String e) {
-
+        if (size < 0) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        elements = Arrays.copyOf(elements, ++size);
+        elements[size()-1] = e;
     }
 
     public void add(int index, String e) {
@@ -26,7 +45,7 @@ public class AList  {
     }
 
     public String get(int index) {
-        return null;
+        return elements[index];
     }
 
     public void set(int index, String e) {
